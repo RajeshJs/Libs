3a4c8b5cccf8e46b2553aa8da777dbd6263cb03d
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-05-18 17:52:45 +0800

    领域模型抽象

---
+++ src/Libs/Domain/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Libs.Domain.Entities
+{
+    public interface IEntity<out TKey> : IId<TKey>
+        where TKey : IEquatable<TKey>
+    {
+
+    }
+}
---
+++ src/Libs/Domain/Entities/IId.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Libs.Domain.Entities
+{
+    public interface IId<out T> where T : IEquatable<T>
+    {
+        T Id { get; }
+    }
+}
